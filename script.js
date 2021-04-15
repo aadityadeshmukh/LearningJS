d1896f8817b17ac6812577d688677b451d130052
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-15 14:06:58 +0530

    2 files modified

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ console.log("hello!");
 // let z = "30";
 // let res = x + y + z;
 // document.getElementById("demo").innerHTML = res;
+console.log("querying button");
+const button = document.querySelector("button");
+console.log("defining onclick");
+button.onclick = function() {
+  let name = prompt("What is your name?");
+  alert("Hello " + name + ", nice to see you!");
+};
