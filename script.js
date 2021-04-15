a8e41d254fd8198107ab4010efcffe6ace062690
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-15 14:29:58 +0530

    2 files modified

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ console.log("hello!");
 // let z = "30";
 // let res = x + y + z;
 // document.getElementById("demo").innerHTML = res;
-console.log("querying button");
-const button = document.querySelector("button");
-console.log("defining onclick");
-button.onclick = function() {
-  let name = prompt("What is your name?");
-  alert("Hello " + name + ", nice to see you!");
-};
+// console.log("querying button");
+// const button = document.querySelector("button");
+// console.log("defining onclick");
+// button.onclick = function() {
+//   let name = prompt("What is your name?");
+//   alert(`Hello ${name} , nice to see you!`);
+// };
+
+let quoteStart = "Don't judge each day by the harvest you reap ";
+let quoteEnd = "but by the seeds you plant.";
+let finalQuote = quoteStart + quoteEnd;
+// Don't edit the code below here!
+const section = document.querySelector("section");
+console.log("creating para");
+let para1 = document.createElement("p");
+para1.textContent = finalQuote;
+
+section.appendChild(para1);
