e6d246129048852501ed9d4ba31f99904d097a7d
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-19 14:58:38 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-console.log("hello!");
-// let x = 10;
-// let y = 20;
-// let z = "30";
-// let res = x + y + z;
-// document.getElementById("demo").innerHTML = res;
-// console.log("querying button");
-const button = document.querySelector("button");
-console.log("defining onclick");
-button.onclick = function() {
-  let name = prompt("What is your name?");
-  if (name == "Admin") {
-    let password = prompt("Enter password:");
-    if (password == "" || password == null) alert("Cancelled");
-    else if (password == "TheMaster") alert("Hello Master");
-    else alert("wrong password holmes!");
-  } else if (name == "" || name == null) {
-    alert("Cancelled");
-  } else {
-    alert("I dont know you!");
+const input = document.querySelector(".numberInput");
+const para = document.querySelector("p");
+
+function squared(num) {
+  return num * num;
+}
+
+function cubed(num) {
+  return num * num * num;
+}
+
+function factorial(num) {
+  if (num < 0) return undefined;
+  if (num == 0) return 1;
+
+  let x = num - 1;
+  while (x > 1) {
+    num *= x;
+    x--;
+  }
+  return num;
+}
+
+input.onchange = function() {
+  const num = input.value;
+  if (isNaN(num)) para.textContent = "Enter a valid number.";
+  else {
+    para.textContent =
+      num +
+      " squared is " +
+      squared(num) +
+      " " +
+      " cubed is " +
+      cubed(num) +
+      " " +
+      " factorial is " +
+      factorial(num);
   }
 };
-
-// let quoteStart = "Don't judge each day by the harvest you reap ";
-// let quoteEnd = "but by the seeds you plant.";
-// let finalQuote = quoteStart + quoteEnd;
-// // Don't edit the code below here!
-// const section = document.querySelector("section");
-// console.log("creating para");
-// let para1 = document.createElement("p");
-// para1.textContent = finalQuote;
-
-// section.appendChild(para1);
-
-// let quote = "I do not like green eggs and ham. I do not like them, Sam-I-Am.";
-// let substring = "green eggs and ham";
-// let lenquote = quote.length;
-// console.log(lenquote);
-// let index = quote.indexOf(substring);
-// console.log(index);
-// let omitString = " I do not like them, Sam-I-Am.";
-// index = quote.indexOf(omitString);
-// finalQuote = quote.substring(0, index);
-// para1.textContent = finalQuote;
-// alert(1 && null && 1);
