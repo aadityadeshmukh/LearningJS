61dec90f50e64b3b52c8a1629a03af4c4564ad8d
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-22 18:06:43 +0530

    1 file modified

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,15 +1,21 @@
 console.log("Hello!");
 function computerPlay() {
+  //Get a random number from 1-3
   let randomSel = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
   let compSel = "";
+  //Assign values to drawn numbers
   if (randomSel == 1) compSel = "Rock";
   else if (randomSel == 2) compSel = "Paper";
   else if (randomSel == 3) compSel = "Scissor";
   return compSel;
 }
+
 function playRound(player, computer) {
+  //Allows users to enter value in any case
   let strPlayer = player.toLowerCase();
   let strComp = computer.toLowerCase();
+  //Check cases. Total 9 cases. 3 are draws.
+  //Computer and Player win for 3 cases each
   if (strPlayer == strComp) {
     return "draw";
   } else {
@@ -27,6 +33,7 @@ function playRound(player, computer) {
     }
   }
 }
+//Activate an input field when play button is clicked and continue play
 let playbtn = document.getElementById("play");
 playbtn.onclick = function() {
   let playerSel = prompt("Enter choice");
