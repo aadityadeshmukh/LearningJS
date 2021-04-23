4859106d3fdd42700650e97359ae935d4a62525c
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-23 14:19:09 +0530

    1 file modified

--- Countdown/script.js
+++ Countdown/script.js
@@ -1,1 +1,13 @@
 console.log("hello");
+let output = document.querySelector(".output");
+output.innerHTML = "";
+for (i = 10; i >= 0; i--) {
+  let paraString = "";
+  if (i == 10) paraString = "Starting countdown";
+  else if (i == 0) paraString = "Blast Off!";
+  else paraString = i;
+
+  let para = document.createElement("p");
+  para.textContent = paraString;
+  output.append(para);
+}
