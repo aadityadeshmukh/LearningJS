cd903e5ae3ff6add2ef4d11a7c14b583bfe1b50d
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-23 17:49:13 +0530

    1 file modified

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ power.onchange = function() {
   const p = power.value;
   answer.textContent = calcPower(b, p);
 };
+
+const reverseString = function(str) {
+  let strFinal = "";
+  let idxSpace = str.indexOf(" ");
+  if (idxSpace > -1) {
+    let arrStr = str.split(" ");
+    for (i = arrStr.length; i > 0; --i) {
+      console.log(i);
+      strFinal += reverSingleWord(arrStr[i - 1]);
+      console.log("final str " + strFinal);
+    }
+  } else strFinal = reverSingleWord(str);
+
+  return strFinal;
+};
+function reverSingleWord(str) {
+  console.log("RSW: " + str);
+  let arrStr = str.split("");
+  let returnStr = "";
+  for (i = arrStr.length - 1; i >= 0; i--) {
+    returnStr += arrStr[i];
+  }
+  console.log("RSW: " + returnStr);
+  return returnStr;
+}
+reverseString("hello there");
