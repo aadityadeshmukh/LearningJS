c3558cae5cbf09dfa053d4228ca2f555ebe072ea
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-26 15:10:30 +0530

    2 files modified

---
+++ DOM-Manipulation/script.js
@@ -0,0 +1,5 @@
+let pElem = document.createElement("p");
+pElem.textContent = "Blue";
+document.body.appendChild(pElem);
+pElem.style.color = "white";
+pElem.style.cssText = "color : white ; background: black";
