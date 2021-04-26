1a00449a151c29b38ebcfad5f242b46f279f761f
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-26 15:15:04 +0530

    1 file modified

--- DOM-Manipulation/script.js
+++ DOM-Manipulation/script.js
@@ -2,3 +2,8 @@ let para = document.createElement("p");
 para.textContent = "I am red";
 document.body.append(para);
 para.style.color = "red";
+
+let head3 = document.createElement("h3");
+head3.textContent = "I am blue h3";
+head3.style.cssText = "color: blue";
+document.body.append(head3);
