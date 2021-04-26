235f79c792bc9e47c71ebfd7e40c5f407692ac9a
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-26 15:23:42 +0530

    1 file modified

--- DOM-Manipulation/script.js
+++ DOM-Manipulation/script.js
@@ -7,3 +7,17 @@ let head3 = document.createElement("h3");
 head3.textContent = "I am blue h3";
 head3.style.cssText = "color: blue";
 document.body.append(head3);
+
+let containerDiv = document.createElement("div");
+containerDiv.classList.add = "container";
+containerDiv.style.cssText =
+  "border: 1px solid black; background-color: pink ; height: 100px";
+document.body.append(containerDiv);
+
+let head1 = document.createElement("h1");
+head1.textContent = "I am inside a div";
+containerDiv.appendChild(head1);
+
+let childPara = document.createElement("p");
+childPara.textContent = "Me too!";
+containerDiv.appendChild(childPara);
