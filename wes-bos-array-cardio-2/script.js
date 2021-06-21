940fd90cfed3f77f5d8503b36b929279d5338ac4
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-06-21 16:37:29 +0530

    implemented the array.every method

--- wes-bos-array-cardio-2/script.js
+++ wes-bos-array-cardio-2/script.js
@@ -1,21 +1,21 @@
-    // ## Array Cardio Day 2
+// ## Array Cardio Day 2
 
-    const people = [
-      { name: 'Wes', year: 1988 },
-      { name: 'Kait', year: 1986 },
-      { name: 'Irv', year: 1970 },
-      { name: 'Lux', year: 2015 }
-    ];
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
 
-    const comments = [
-      { text: 'Love this!', id: 523423 },
-      { text: 'Super good', id: 823423 },
-      { text: 'You are the best', id: 2039842 },
-      { text: 'Ramen is my fav food ever', id: 123523 },
-      { text: 'Nice Nice Nice!', id: 542328 }
-    ];
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
 
-    // Some and Every Checks
+// Some and Every Checks
 
 // Array.prototype.some() // is at least one person 19 or older?
 let isAdult = people.some(function(person) {
@@ -23,3 +23,10 @@ let isAdult = people.some(function(person) {
   if (currYear - person.year >= 19) return true;
 });
 console.log({ isAdult });
+
+// Array.prototype.every() // is everyone 19 or older?
+let allAdults = people.every(person => {
+  let currYear = new Date().getFullYear();
+  if (currYear - people.year >= 19) return true;
+});
+console.log({ allAdults });
