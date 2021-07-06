97a8f59617666784e597b9d82ab74ceeff4d0919
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-07-06 15:10:51 +0530

    3 files modified

--- Book-store/script.js
+++ Book-store/script.js
@@ -1,1 +1,15 @@
 console.log('book store');
+console.log('bookstore');
+function bookProfile(title, author, numPages, status) {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  if (status === false) this.status = 'Not read yet.';
+  else this.status = 'Done reading.';
+  this.info = function() {
+    return title + ', ' + author + ', ' + numPages + ', ' + this.status;
+  };
+}
+
+book = new bookProfile('Blue', 'Blah', '10000', false);
+console.log(book.info());
